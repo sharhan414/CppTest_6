f2ac3fe38b7d4322d610ed317b8f1a93fdd45ad3
sharhan414 <ssoltygov98@mail.ru>
2021-10-02 20:29:56 +0300

    add new thread

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
+#include <thread>
+
 using namespace std;
+static bool FuncThreadCont=true;
+void func(){
+    int32_t Timer=0;
+    while(FuncThreadCont){
+        this_thread::sleep_for(chrono::milliseconds(100));
+cout<<"Timer:"<<++Timer<<endl;
+    }
+
+}
 
 int main(){
 
+
+thread FuncTread(func);
+
 cout<<"Hello, world!"<<endl;
 
+FuncTread.join();
 
 return  0;
 }
